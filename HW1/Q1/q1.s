48c7d898d85367ad1422f23e4faaa70288b9f415
garry <t110590028@ntut.edu.tw>
2024-09-26 11:17:14 +0800

    q1 version 2

--- HW1/Q1/q1.s
+++ HW1/Q1/q1.s
@@ -1,16 +1,17 @@
-.data                     # 資料段
-    msg: .string "n = %d\n"   # 定義格式字串，尾部自動加上 0 字符
+.data                     # data
+    msg: .string "n = %d\n"   # define format, and add 0(\n)
 
-.text                     # 程式碼段
-    .globl main               # 定義全域主函數
-    .extern printf            # 宣告外部的 printf 函數
+.text                     # code section
+    .globl main               # define main(global)
+    .extern printf            # define external func(printf)
 
 main:
-  push %rbx                   # 保存 %rbx 的值
-  lea  msg(%rip), %rdi      # 加載格式字串的地址到 %rdi
-  mov  $42, %rsi               # 將 42 (數值) 存入 %esi，會自動擴展到 %rsi
-  xor %rax, %rax              # 高效清空 %eax
-  call printf                 # 呼叫 printf 函數
-  pop %rbx                    # 恢復 %rbx 的原始值
-  xor %rax, %rax        # 將 %rax 清空並設置為 0，表示成功退出
-  ret                         # 返回
+  push %rbx                   # push rbx
+  lea  msg(%rip), %rdi      # load format string to rdi(the first argu)
+  ; mov $msg, %rdi  # or can do it.
+  mov  $42, %rsi               # load the 42 to rsi(the second argu)
+  xor %rax, %rax              # let rax to 0
+  call printf                 # call printf
+  pop %rbx                    # pop rbx, let rbx recover
+  xor %rax, %rax        # this step let rax to 0, printf will return the number of string that success echo.
+  ret                         # return
